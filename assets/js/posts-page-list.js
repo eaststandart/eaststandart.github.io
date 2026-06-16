d83477225aa1147ff19257814819fa12b71755f4
eaststandart <eaststandart@gmail.com>
2026-06-16 22:29:03 +1000

    vault backup: 2026-06-16 22:29:03

--- assets/js/posts-page-list.js
+++ assets/js/posts-page-list.js
@@ -23,17 +23,16 @@ function runMediaArchiveFilter() {
         }
     });
 
-
-    // 2. ЖЕЛЕЗОБЕТОННАЯ ЗАЧИСТКА ХВОСТОВ: Находим последний ВИДИМЫЙ пост на экране
-    var visibleEntries = Array.from(document.querySelectorAll('.media-archive-list-wrapper .media-entry')).filter(function(el) {
-      return el.style.display === 'block';
-    });
-
+    // 2. ЗАЧИСТКА ХВОСТОВ: Корректируем стили для последнего оставшегося поста
+    var visibleEntries = document.querySelectorAll('.media-archive-list-wrapper .media-entry');
+    
     if (visibleEntries.length > 0) {
-      var lastItem = visibleEntries[visibleEntries.length - 1];
-      // Полностью обнуляем маргин и прячем разделительную линию у финишного поста
-      lastItem.style.setProperty('margin-bottom', '0px', 'important');
-      var hr = lastItem.querySelector('.media-entry-hr');
-      if (hr) hr.style.setProperty('display', 'none', 'important');
+        var lastItem = visibleEntries[visibleEntries.length - 1];
+        lastItem.style.setProperty('margin-bottom', '0px', 'important');
+        
+        var hr = lastItem.querySelector('.media-entry-hr');
+        if (hr) {
+            hr.style.setProperty('display', 'none', 'important');
+        }
     }
 }

0f218c405615e0d0fc55ef8fb5c57ee9f093ee5f
eaststandart <eaststandart@gmail.com>
2026-09-21 23:33:24 +1000

    vault backup: 2026-09-21 23:33:24

--- assets/js/pagination-logic.js
+++ assets/js/pagination-logic.js
@@ -24,7 +24,6 @@ function runPagination(listId, controlsId, itemsPerPage) {
   var feedData = null;
 
   // 1. МГНОВЕННЫЙ ФОНОВЫЙ ЗАПРОС К КВАНТОВАННОЙ БАЗЕ ДАННЫХ
-  // var feedUrl = window.location.origin + '/_data/feed.yml';
   var feedUrl = window.location.origin + '/assets/feed.yml';
   
   fetch(feedUrl)
